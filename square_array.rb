639338f3c1427e048b6cb6eb07cfbe2853f963d3
slicky1 <okunonke@gmail.com>
2021-07-05 16:19:58 -0400

    done

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+# This also works:
+# def square_array(array)
+#   array.collect{|et| et ** 2}
+# end
+
 def square_array(array)
-  # your code here
+  
+  returns = []
+  array.each{|et| returns.push( et * et )}
+  returns
 end
